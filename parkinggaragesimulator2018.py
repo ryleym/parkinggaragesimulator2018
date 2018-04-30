f3795e9f9800fda1e2210dc3347e66062a59b83c
rmatos42 <ryleymatos@gmail.com>
2018-04-29 22:40:52 -0700

    waddup

--- parkinggaragesimulator2018.py
+++ parkinggaragesimulator2018.py
@@ -10,17 +10,17 @@ cars = []
 x = []
 y = []
 curr_time = datetime.now();
-close_time = curr_time + timedelta(hours=6);
+close_time = curr_time + timedelta(hours=18);
 print('close_time: ', close_time);
 while (datetime.now().timestamp() <= close_time.timestamp() or len(cars) != 0):
 	entry = randint(0,1)
-	if (entry == 1 and len(cars) != 500):
+	if (entry == 1 and len(cars) != 1000):
 		tag = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(6)]);
 		entry_time = datetime.now();
-		exit_time = entry_time + timedelta(hours=uniform(0, 6))
+		exit_time = entry_time + timedelta(hours=uniform(0, 18))
 		if (exit_time.timestamp() <= close_time.timestamp()):
 			cars.append({'parking_tag': tag, 'entry_time': entry_time, 'exit_time': exit_time});
-			requests.post('http://zero5.co:3002/garages/5ac51c8ebc690d1425bcba7c/cars', {'parking_tag': tag});
+			requests.post('http://zero5.co:3002/garages/5ae68dba20242561359123ec/cars', {'parking_tag': tag});
 			x.append(datetime.now());
 			y.append(len(cars));
 		#print('cars: ')
@@ -30,7 +30,7 @@ while (datetime.now().timestamp() <= close_time.timestamp() or len(cars) != 0):
 	for car in cars:
 		if (car['exit_time'].timestamp() < datetime.now().timestamp()):
 			i += 1;
-			requests.put('http://zero5.co:3002/garages/5ac51c8ebc690d1425bcba7c/cars', {'parking_tag': car['parking_tag']});
+			requests.put('http://zero5.co:3002/garages/5ae68dba20242561359123ec/cars', {'parking_tag': car['parking_tag']});
 			x.append(datetime.now());
 			y.append(len(cars) - i);
 	cars = [car for car in cars if car['exit_time'].timestamp() >= datetime.now().timestamp()]
